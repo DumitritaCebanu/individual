eab701b9a6e4f970e89eaec46ba580acc2a9a6c2
Cebanu Dumitrita <dumitrita.cebanu.99@gmail.com>
2020-03-19 14:34:55 +0200

    made change in newBranch

--- lab1-master/src/loose/oose/fis/Main.java
+++ lab1-master/src/loose/oose/fis/Main.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+	System.out.println("make a change");
 
         String[] xmlList = new String[6];
         xmlList[0] = "<tag1>";
---
+++ lab1-master/src/loose/oose/fis/processors/Asd.java
@@ -0,0 +1,8 @@
+package loose.oose.fis.processors;
+
+import loose.oose.fis.documents.Document;
+
+
+public class Asd {
+    System.out.println("Something");
+}
